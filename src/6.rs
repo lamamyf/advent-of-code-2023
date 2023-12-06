816785c18d3ac7e1d7382bb35bfc0e8eccbfbbd3
Lama AlMayouf <lamayf@hotmail.com>
2023-12-06 18:23:42 +0300

    day 3 part 2

---
+++ src/6.rs
@@ -0,0 +1,89 @@
+use std::collections::HashSet;
+use std::fs::read_to_string;
+
+fn main() {
+    let input = read_lines("input.txt");
+    let mut numbers: Vec<Number> = Vec::new();
+    let mut symbols: HashSet<(i32, i32)> = HashSet::new();
+
+    let mut current_number: Option<Number> = None;
+
+    for (row, line) in input.iter().enumerate() {
+        for (col, c) in line.chars().enumerate() {
+           if c.is_numeric() {
+               if let Some(ref mut n) = current_number {
+                   n.add_digit((c as u8 - b'0') as i32, row as i32, col as i32);
+               } else {
+                   current_number = Some(Number::new((c as u8 - b'0') as i32, row as i32, col as i32));
+               }
+           } else  {
+               if let Some(n) = current_number.take() {
+                   numbers.push(n);
+                   current_number = None;
+               }
+
+               if c == '*' {
+                   symbols.insert((row as i32, col as i32));
+               }
+           }
+        }
+    }
+
+    if let Some(n) = current_number.take() {
+        numbers.push(n);
+    }
+
+    let mut sum = 0;
+
+    for s in &symbols {
+        let intersection: Vec<i32> = numbers.iter().filter(|n| n.surrounding_coordinates.contains(s)).map(|n| n.value).collect();
+        if intersection.len() == 2 {
+            sum += intersection.iter().product::<i32>();
+        }
+
+    }
+
+    dbg!(sum);
+}
+
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()  // gather them together into a vector
+}
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+    surrounding_coordinates: HashSet<(i32, i32)>
+}
+
+impl Number {
+    fn new(value: i32, row: i32, col: i32) -> Self {
+     Self {
+         value,
+         surrounding_coordinates: Self::get_surrounding_coordinates(row, col)
+     }
+    }
+
+    fn add_digit(&mut self, digit: i32, row: i32, col: i32) {
+        self.value = self.value * 10 + digit;
+        self.surrounding_coordinates.extend(Self::get_surrounding_coordinates(row, col));
+    }
+
+    fn get_surrounding_coordinates(row: i32, col: i32) -> HashSet<(i32, i32)> {
+        HashSet::from([
+            (row, col - 1),
+            (row, col + 1),
+            (row - 1, col),
+            (row + 1, col),
+            (row - 1, col - 1),
+            (row + 1, col + 1),
+            (row + 1, col - 1),
+            (row - 1, col + 1)
+        ])
+    }
+}
+
